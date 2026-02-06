108e499ae874a1990fdc198816ce8ba73e6a185c
sahilnikalje <sahilnik88@gmail.com>
2026-02-06 13:59:45 +0530

    burger image added

--- Backend/data/menu.seed.js
+++ Backend/data/menu.seed.js
@@ -18,7 +18,7 @@ const seedMenu=async()=>{
       name: "Veg Burger",
       description: "Crispy veg patty with lettuce",
       price: 149,
-      image: "/images/burger.png"
+      image: "/images/burger.jpg"
     },
     {
       name: "Pasta",
